817f95aaa9ce44184b99fe2bea4ccfb48c8a74a0
natinael96 <117823053+natinael96@users.noreply.github.com>
2024-02-15 15:59:57 +0300

    Add solution for Plus One

---
+++ camp/leetcode/plus-one.py
@@ -0,0 +1,7 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = 0
+        for i in digits:
+            num = num*10 + i
+        num += 1
+        return [int(x) for x in str(num)]
