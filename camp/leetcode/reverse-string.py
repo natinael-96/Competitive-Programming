ad0f8fac11fcf04607871b81b71f2888d42f7518
natinael96 <117823053+natinael96@users.noreply.github.com>
2024-02-20 22:17:50 +0300

    Add solution for Reverse String

--- camp/leetcode/reverse-string.py
+++ camp/leetcode/reverse-string.py
@@ -1,16 +1,14 @@
-from typing import List
-
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        def recurSoln(strng, i):
-            n = len(strng)
-            if i == n // 2:
-                return
-            else:
-                strng[i], strng[n - i - 1] = strng[n - i - 1], strng[i]
-                recurSoln(strng, i + 1)
         
-        recurSoln(s, 0)
+        def rev(strg, l):
+            if l == len(strg)//2:
+                return 
+            
+            strg[l], strg[len(strg) - l -1] = strg[len(strg) - l -1], strg[l]
+            
+            rev(strg, l + 1)
+        rev(s, 0)
