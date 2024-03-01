9faaab78e7b16edf7b6bace2bf10a54e9bece946
natinael96 <117823053+natinael96@users.noreply.github.com>
2024-03-01 06:50:37 +0300

    Add solution for Matrix Diagonal Sum

---
+++ camp/leetcode/matrix-diagonal-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        ans = 0
+        for i in range(n):
+            ans += mat[i][i]
+            if i != n - i - 1:
+                ans += mat[i][n - i - 1]
+        return ans
