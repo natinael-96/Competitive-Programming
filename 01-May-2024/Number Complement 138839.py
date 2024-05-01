1aa04b9e96c79287faab671fba9dd277cfdc83ba
natinael96 <117823053+natinael96@users.noreply.github.com>
2024-05-01 23:42:56 +0300

    Created 'Number Complement' by A2SV Hub

---
+++ 01-May-2024/Number Complement 138839.py
@@ -0,0 +1,8 @@
+# Problem: Number Complement - https://leetcode.com/problems/number-complement/
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        mask = 1
+        while mask < num:
+            mask = (mask << 1) + 1
+        return num ^ mask
