d80a0ac58867180b55839e02d7317d06f4b7e896
natinael96 <117823053+natinael96@users.noreply.github.com>
2024-05-06 11:08:04 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 06-May-2024/Cirno's Perfect Bitmasks Classroom 140961.py
@@ -0,0 +1,8 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+for _ in range(int(input())):
+    x = int(input())
+    y = x & (-x)
+    while  (not (y ^ x) or not(y & x)):
+        y += 1
+    print(y)
