0fd8fb6360e2c69d03a2c77c1037c54586a76b43
natinael96 <117823053+natinael96@users.noreply.github.com>
2024-07-28 10:28:32 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 28-Jul-2024/B - Mohammed's Magical Cleaning Machine 185349.py
@@ -0,0 +1,11 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    mx = 0
+    for i in arr[:-1]:
+        if i or mx > i:
+            mx += i + int(i == 0)
+    print(mx)
